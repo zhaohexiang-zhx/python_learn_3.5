d26c9e120ea0f7d15bc55f162f63a718a6efe8c5
zhaohexiang-zhx <zhx190523@163.com>
2021-02-19 17:09:54 +0800

    logging name

--- src/modules/1-loggingexample.py
+++ src/modules/1-loggingexample.py
@@ -6,7 +6,8 @@ from logging.handlers import RotatingFileHandler
 
 def log_basic():
     # CRITICAL 50; ERROR 40; WARNING 30; INFO 20; DEBUG 10; NOSET 0;
-    logging.basicConfig(filename="web.log", level=0,
+    # filename-指定日志输出文件的文件名; format-指定日志格式字符串，
+    logging.basicConfig(filename="1-web.log", level=0,
                         format='%(asctime)s %(name)s %(levelname)s %(module)s:%(lineno)d %(message)s')
     logging.getLogger("paramiko").setLevel(logging.WARNING)
     logging.getLogger("selenium").setLevel(logging.WARNING)
@@ -19,7 +20,7 @@ def log_rorate():
     logging.basicConfig(level=20, format='%(asctime)s %(name)s %(levelname)s %(module)s:%(lineno)d %(message)s')
 
     # 定义RotatingFileHandler，且文件大小超过10M时进行备份，最多5个备份日志文件
-    rotateHandler = RotatingFileHandler('you.log', maxBytes=1024*0.01, backupCount=5)
+    rotateHandler = RotatingFileHandler('1-you.log', maxBytes=1024 * 0.01, backupCount=5)
     rotateHandler.setFormatter(logging.Formatter(format))
     logging.getLogger("").addHandler(rotateHandler)
     logging.getLogger().propagate = False
