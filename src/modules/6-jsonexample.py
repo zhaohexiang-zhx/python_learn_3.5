8180b616e0e5a8819cd7d63f1d4584460c247116
zhaohexiang-zhx <zhx190523@163.com>
2021-02-20 14:04:07 +0800

    json模块

---
+++ src/modules/6-jsonexample.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import json
+
+python_dict = {'name': 'shuxue', u'sex': 'Male', 'c': 3, 'd': 4, 'e': 5}
+print(type(json.dumps(python_dict)))
+print(json.dumps(python_dict).encode('utf-8'))
+
+print('*'*50)
+
+json_str = '{"name": "shuxue", "sex": "Male", "c": 3, "d": 4, "e": 5}'
+print(type(json.loads(json_str)))
+print(json.loads(json_str))
+
