38a008c32082512b11f441b9eeb8fd0caf130cfb
zhaohexiang-zhx <zhx190523@163.com>
2021-02-20 10:53:11 +0800

    gitignore添加

---
+++ src/modules/4-shutilexample.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from shutil import make_archive
+import os
+def targz_dir():
+    archive_name = "myimage"
+    root_dir = "../image"
+    make_archive(archive_name, 'gztar', root_dir)
+
+if __name__ == "__main__":
+    targz_dir()
