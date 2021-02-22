3ec2da408e91de0a33bb5f711d671f8904b00f9b
zhaohexiang-zhx <zhx190523@163.com>
2021-02-22 09:35:44 +0800

    json

--- src/modules/6-jsonexample.py
+++ src/modules/6-jsonexample.py
@@ -4,11 +4,13 @@
 import json
 
 python_dict = {'name': 'shuxue', u'sex': 'Male', 'c': 3, 'd': 4, 'e': 5}
+# dumps
 print(type(json.dumps(python_dict)))
 print(json.dumps(python_dict).encode('utf-8'))
 
 print('*'*50)
 
+# loads
 json_str = '{"name": "shuxue", "sex": "Male", "c": 3, "d": 4, "e": 5}'
 print(type(json.loads(json_str)))
 print(json.loads(json_str))
